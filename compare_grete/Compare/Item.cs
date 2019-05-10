0452b43bac611df6db6d03098e0299449d0b9c46
GreteO <ojagre@tlu.ee>
2019-05-10 10:52:24 +0300

    Added result comparing

---
+++ compare_grete/Compare/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Compare
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Distance { get; set; }
+        public int Speed { get; set; }
+
+        private string newName = "New Input:";
+        public string NewName
+        {
+            get { return newName; }
+            set { if (value != null) newName = value; }
+        }
+
+        private int newDistance = 7;
+        public int NewDistance
+        {
+            get { return newDistance; }
+            set { if (value != 0) newDistance = value; }
+        }
+
+        private int newSpeed = 15;
+        public int NewSpeed
+        {
+            get { return newSpeed; }
+            set { if (value != 0) newSpeed = value; }
+        }
+
+        public string NewData { get { return NewName + " Distance: " + NewDistance.ToString() + " Speed: " + NewSpeed.ToString(); } }
+    }
+}
